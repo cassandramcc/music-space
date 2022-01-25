2f3b2b4aad680f3a5dc569a3aa4bff5eaa14978e
Cassandra <cassandra.tablet55@gmail.com>
2022-01-25 20:58:51 +0000

    fixed cube UI element

--- music-space/Assets/Scripts/CubeSpawner.cs
+++ music-space/Assets/Scripts/CubeSpawner.cs
@@ -18,12 +18,14 @@ public class CubeSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 spawnPosition = (camera.transform.forward + new Vector3(0,0.4f,0)) * 5f + transform.position;
+        cubeUIElement.transform.position = spawnPosition;
         if (Input.GetMouseButtonDown(0)){
             Debug.Log("Mouse pressed");
             //Vector3 spawnPosition = transform.forward * 2f + transform.position;
-            Vector3 spawnPosition = (camera.transform.forward + new Vector3(0,0.4f,0)) * 5f + transform.position;
+            
             Instantiate(musicCube, spawnPosition, Quaternion.identity);
-            cubeUIElement.transform.position = spawnPosition + new Vector3(0,0.5f,0);
+            
         }
     }
 }
