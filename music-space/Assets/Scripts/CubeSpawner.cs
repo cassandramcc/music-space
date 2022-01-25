dbc7294063981ca63c80319c9473b09c641dbe93
Cassandra <cassandra.tablet55@gmail.com>
2022-01-25 17:40:14 +0000

    Making UI element

--- music-space/Assets/Scripts/CubeSpawner.cs
+++ music-space/Assets/Scripts/CubeSpawner.cs
@@ -7,6 +7,7 @@ public class CubeSpawner : MonoBehaviour
 
     public GameObject musicCube;
     public GameObject camera;
+    public GameObject cubeUIElement;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,7 @@ public class CubeSpawner : MonoBehaviour
             //Vector3 spawnPosition = transform.forward * 2f + transform.position;
             Vector3 spawnPosition = (camera.transform.forward + new Vector3(0,0.4f,0)) * 5f + transform.position;
             Instantiate(musicCube, spawnPosition, Quaternion.identity);
+            cubeUIElement.transform.position = spawnPosition + new Vector3(0,0.5f,0);
         }
     }
 }
