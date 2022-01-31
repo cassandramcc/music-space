d1407a6f0b72f02de619743b4eb49de5a7ef5bad
Cassandra <cassandra.tablet55@gmail.com>
2022-01-31 17:25:46 +0000

    Meshes draw on player's mouse, but aren't separate pieces and sometimes faces are wrong way

--- music-space/Assets/PaintCreator.cs
+++ music-space/Assets/PaintCreator.cs
@@ -7,8 +7,8 @@ public class PaintCreator : MonoBehaviour
 {
     Mesh mesh;
     List<Vector3> vertices;
+    int newestVertexDrawn = 0;
     List<int> triangles;
-
     public Camera cam;
 
     public GameObject mouseCursorUI;
@@ -16,76 +16,80 @@ public class PaintCreator : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
 
-        CreateShape();
         UpdateMesh();
     }
 
-    void CreateShape(){
-        vertices = new List<Vector3>();
-        triangles = new List<int>();
-        for(int i = 0; i <= 10; i++){
-            vertices.Add(new Vector3(0,0,i));
-            vertices.Add(new Vector3(0,1,i));
-            vertices.Add(new Vector3(1,1,i));
-            vertices.Add(new Vector3(1,0,i));
-        }
-        int[] frontFace = {0,1,2,0,2,3};
-        triangles.AddRange(frontFace);
-        for(int i = 0; i < 10; i++){
-            int corner = i*4;
-            int[] bottom1 = {corner,corner+7,corner+4};
-            int[] bottom2 = {corner,corner+3,corner+7};
-            int[] left1 = {corner,corner+5,corner+1};
-            int[] left2 = {corner,corner+4,corner+5};
-            int[] top1 = {corner+1,corner+5,corner+6};
-            int[] top2 = {corner+1,corner+6,corner+2};
-            int[] right1 = {corner+3,corner+2,corner+6};
-            int[] right2 = {corner+3,corner+6,corner+7};
-            triangles.AddRange(bottom1);
-            triangles.AddRange(bottom2);
-            triangles.AddRange(left1);
-            triangles.AddRange(left2);
-            triangles.AddRange(top1);
-            triangles.AddRange(top2);
-            triangles.AddRange(right1);
-            triangles.AddRange(right2);
-        }
-    }
 
     void UpdateMesh(){
         mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
+        newestVertexDrawn = vertices.Count;
     }
 
+    void drawVertices(){
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+        DebugSpheres(point);
+        Vector3[] newVertices = {
+            new Vector3(point.x,point.y,point.z),
+            new Vector3(point.x,point.y+1,point.z),
+            new Vector3(point.x,point.y+1,point.z+1),
+            new Vector3(point.x,point.y,point.z+1)
+        };
+        vertices.AddRange(newVertices);
+    }
+
+    void drawTriangles(){
+        for (int i = 0; i < (int)(vertices.Count-8)/4; i++){
+            int vertexIndex = i*4;
+            int[] newTriangles = {
+                //left
+                vertexIndex,vertexIndex+1,vertexIndex+5,
+                vertexIndex,vertexIndex+5,vertexIndex+4,
+                //right
+                vertexIndex+6,vertexIndex+3,vertexIndex+7,
+                vertexIndex+2,vertexIndex+3,vertexIndex+6,
+                //top
+                vertexIndex+1,vertexIndex+2,vertexIndex+6,
+                vertexIndex+1,vertexIndex+6,vertexIndex+5,
+                //bottom
+                vertexIndex,vertexIndex+7,vertexIndex+3,
+                vertexIndex,vertexIndex+4,vertexIndex+7
+            };
+            triangles.AddRange(newTriangles);
+        }
+        //front face
+        triangles.AddRange(new int[]{newestVertexDrawn,newestVertexDrawn+3,newestVertexDrawn+1,newestVertexDrawn+3,newestVertexDrawn+2,newestVertexDrawn+1});
+        //end face
+        int endIndex = vertices.Count - 4;
+        triangles.AddRange(new int[]{endIndex,endIndex+1,endIndex+2,endIndex,endIndex+2,endIndex+3});
+        UpdateMesh();
+    }
     void Update() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
         mouseCursorUI.transform.position = point;
-        Debug.Log(point);
-        if (Input.GetMouseButtonDown(1)){
-            DebugSpheres(point);
-            Vector3[] newVertices = {
-                new Vector3(point.x,point.y,point.z),
-                new Vector3(point.x,point.y+1,point.z),
-                new Vector3(point.x+1,point.y+1,point.z),
-                new Vector3(point.x+1,point.y,point.z)
-            };
-            vertices.AddRange(newVertices);
+        Debug.DrawRay(cam.transform.position,cam.transform.forward*10,Color.green);
 
-            int recentVertices = vertices.Count-4;
-            int[] newTriangles = {
-                
-                recentVertices,recentVertices+1,recentVertices+2,
-                recentVertices,recentVertices+2,recentVertices+3
-            };
-            triangles.AddRange(newTriangles);
-            UpdateMesh();
+        if (Input.GetMouseButton(1)){
+            drawVertices();
+            Debug.Log("Vertices");
+            Debug.Log(vertices.Count);
+            Debug.Log(newestVertexDrawn);
         }
-
-        void DebugSpheres(Vector3 point){
+        else if (Input.GetMouseButtonUp(1)){
+            drawTriangles();
+            Debug.Log("Triangles");
+            Debug.Log(vertices.Count);
+            Debug.Log(newestVertexDrawn);
+        }
+    }
+    void DebugSpheres(Vector3 point){
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             cube.transform.position = new Vector3(point.x,point.y,point.z);
             cube.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
@@ -93,12 +97,11 @@ public class PaintCreator : MonoBehaviour
             cube.transform.position = new Vector3(point.x,point.y+1,point.z);
             cube.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
             cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            cube.transform.position = new Vector3(point.x+1,point.y+1,point.z);
+            cube.transform.position = new Vector3(point.x,point.y+1,point.z+1);
             cube.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
             cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            cube.transform.position = new Vector3(point.x+1,point.y,point.z);
+            cube.transform.position = new Vector3(point.x,point.y,point.z+1);
             cube.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
         }
-    }
 
 }
