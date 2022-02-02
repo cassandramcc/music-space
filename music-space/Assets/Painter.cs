542f747bdeab9993d4174646ff56c6d4da5db6d2
Cassandra <cassandra.tablet55@gmail.com>
2022-02-02 18:17:43 +0000

    A TUBE!!!!

--- music-space/Assets/Painter.cs
+++ music-space/Assets/Painter.cs
@@ -7,9 +7,12 @@ public class Painter : MonoBehaviour
 
     class Vertex{
         public Vector3 pos;
-        public Vector3[] edgeVertices = new Vector3[]{};
+        public List<Vector3> edgeVertices = new List<Vector3>();
 
         public Vector3 direction;
+        public Vector3 orthoDirection;
+
+        public Vector3 rotated;
         public Vertex(Vector3 _pos){
             pos = _pos;
         }
@@ -19,7 +22,7 @@ public class Painter : MonoBehaviour
     Mesh mesh;
     List<Vector3> vertices;
 
-    List<Vertex> centralVertices;
+    List<Vertex> centralVertices = new List<Vertex>();
     int newestVertexDrawn = 0;
     List<int> triangles;
 
@@ -50,6 +53,7 @@ public class Painter : MonoBehaviour
         mouseCursorUI.transform.position = point;
         Debug.DrawRay(cam.transform.position,cam.transform.forward*10,Color.green);
         centralVertices.Add(new Vertex(point));
+        DebugSpheres(point);
     }
 
     void CalculateVertexDirection(){
@@ -58,11 +62,27 @@ public class Painter : MonoBehaviour
             direction = direction/direction.magnitude; //normalised direction
             Vertex lastVertex = centralVertices[centralVertices.Count - 2];
             lastVertex.direction = direction;
+            Debug.DrawRay(lastVertex.pos,lastVertex.direction*2f,Color.red);
             //this is the direction between first vertex and the next vertex
             //the first vertex's points need to be in the perpedincular direction
+            Vector3 perpendicular = new Vector3();
+            Vector3.OrthoNormalize(ref direction,ref perpendicular);
+            lastVertex.orthoDirection = perpendicular;
+            Debug.DrawRay(lastVertex.pos,perpendicular*5,Color.cyan);
+            Vector3 rotation = perpendicular * Mathf.Cos(Mathf.PI/2) + (Vector3.Cross(direction,perpendicular))*Mathf.Sin(Mathf.PI/2) + direction*(Vector3.Dot(perpendicular,direction))*(1-Mathf.Cos(Mathf.PI/2));
+            lastVertex.rotated = rotation;
+            Debug.DrawRay(lastVertex.pos,rotation*2f,Color.magenta);
+            
             //The perpendicular direction is the negative reciprical gradient, so need to calculate gradient of direction
             // then turn perpendicular gradient into a vector direction.
             //Create points on plane defined by this perpendicular gradient
+            for (int i = 0; i <= 270; i+=90){
+                Vector3 rot = perpendicular * Mathf.Cos(i * Mathf.Deg2Rad) + (Vector3.Cross(direction,perpendicular))*Mathf.Sin(i * Mathf.Deg2Rad) + direction*(Vector3.Dot(perpendicular,direction))*(1-Mathf.Cos(i * Mathf.Deg2Rad));
+                Vector3 newPoint = lastVertex.pos - rot;
+                Vector3 shrinkNewPoint = Vector3.MoveTowards(newPoint,lastVertex.pos,0.5f);
+                lastVertex.edgeVertices.Add(shrinkNewPoint);
+                DebugSpheres(shrinkNewPoint);
+            }
             //Before doing triangles, add debug spheres to test points spawn correctly.
         }
     }
@@ -75,8 +95,26 @@ public class Painter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DrawPoint();
-        CalculateVertexDirection();
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+        mouseCursorUI.transform.position = point;
+        if (Input.GetMouseButton(0)){
+            DrawPoint();
+            CalculateVertexDirection();
+        }
+        foreach (Vertex v in centralVertices){
+            Debug.DrawRay(v.pos,v.direction*0.4f,Color.red);
+            Debug.DrawRay(v.pos,v.orthoDirection*0.5f,Color.cyan);
+            Debug.DrawRay(v.pos,v.rotated*0.5f,Color.magenta);
+        }
+        
+        
         DrawTriangles();
+    }
+
+    void DebugSpheres(Vector3 point){
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cube.transform.position = new Vector3(point.x,point.y,point.z);
+        cube.transform.localScale = new Vector3(0.2f,0.2f,0.2f);
     }
 }
