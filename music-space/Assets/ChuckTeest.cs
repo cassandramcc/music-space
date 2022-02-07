ce37066a0bd438fe3415e0afdc3470151a94c004
Cassandra <cassandra.tablet55@gmail.com>
2022-02-07 18:35:55 +0000

    Chuck for audio

---
+++ music-space/Assets/ChuckTeest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChuckTeest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ChuckSubInstance>().RunCode(@"
+            SinOsc foo => dac;
+            while( true )
+            {
+                Math.random2f( 300, 1000 ) => foo.freq;
+                100::ms => now;
+            }
+	    ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
