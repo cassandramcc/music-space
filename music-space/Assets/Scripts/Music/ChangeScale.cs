f20e1fd9811e11aa54eb825e94a0e8c6c0a9b8f7
casster <od18309@bristol.ac.uk>
2022-04-13 15:56:31 +0100

    Scale UI text

--- music-space/Assets/Scripts/Music/ChangeScale.cs
+++ music-space/Assets/Scripts/Music/ChangeScale.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Threading;
 
 public class ChangeScale : MonoBehaviour
 {
     public GameObject painter;
+    public TextMeshPro scaleUI;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +21,24 @@ public class ChangeScale : MonoBehaviour
         
     }
 
+    void PopUpScaleUI()
+    {
+        scaleUI.gameObject.SetActive(true);
+        scaleUI.SetText(painter.GetComponent<Painter>().currentScale.ToString());
+        Invoke("DisableScaleUI", 4f); 
+    }
+
+    void DisableScaleUI()
+    {
+        scaleUI.gameObject.SetActive(false);
+    }
+
     public void DownScale(){
         painter.GetComponent<Painter>().currentScale = (painter.GetComponent<Painter>().currentScale - 1);
         if (painter.GetComponent<Painter>().currentScale < 0) {
             painter.GetComponent<Painter>().currentScale = 0;
         }
+        PopUpScaleUI();
     }
 
     public void UpScale(){
@@ -29,5 +46,6 @@ public class ChangeScale : MonoBehaviour
         if (painter.GetComponent<Painter>().currentScale > Root.FMajor) {
             painter.GetComponent<Painter>().currentScale = Root.FMajor;
         }
+        PopUpScaleUI();
     }
 }
